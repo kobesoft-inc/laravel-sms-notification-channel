69af4223e89bb416aaab9afea668ad6e9712975d
ks-jseo <j.seo@kobesoft.co.jp>
2025-04-11 17:09:40 +0900

    Update RakutenGateway.php

--- src/Gateway/RakutenGateway.php
+++ src/Gateway/RakutenGateway.php
@@ -10,32 +10,33 @@ class RakutenGateway implements GatewayInterface
 {
     protected string $apiKey;
     protected string $endpoint;
+    protected string $from;
     protected ?ClientInterface $http;
     protected ?RequestFactoryInterface $requestFactory;
     protected ?StreamFactoryInterface $streamFactory;
 
     public function __construct(
-        string $apiKey,
-        string $endpoint,
         ClientInterface $http = null,
         RequestFactoryInterface $requestFactory = null,
         StreamFactoryInterface $streamFactory = null
     ) {
-        $this->apiKey = $apiKey;
-        $this->endpoint = $endpoint;
+        $this->apiKey = config('services.rakuten.api_key');
+        $this->endpoint = config('services.rakuten.api_endpoint');
+        $this->from = config('services.rakuten.from');
+
         $this->http = $http;
         $this->requestFactory = $requestFactory;
         $this->streamFactory = $streamFactory;
     }
 
-    public function send(string $to, string $from, string $message): void
+    public function send(string $to, string $message): void
     {
         if (!$this->http || !$this->requestFactory || !$this->streamFactory) {
             throw new \RuntimeException('HTTP client, request factory, and stream factory are required.');
         }
 
         $body = json_encode([
-            'from'    => $from,
+            'from'    => $this->from,
             'to'      => $to,
             'message' => $message,
         ]);
